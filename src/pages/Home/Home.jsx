35202af9f3cb92ff9003f0beeffe207338804815
“G-Gakii” <gakiiviolet1@gmail.com>
2025-06-23 15:40:49 +0300

    mission component

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,7 +3,7 @@ import Navbar from "../../components/Navbar/Navbar";
 import Hero from "../../components/hero/Hero";
 import styles from "./Home.module.css";
 import About from "../../components/About/About";
-import Mission from "../../components/mission/Mission";
+import Mission from "../../components/Mission/Mission";
 import Projects from "../../components/Projects/Projects";
 import Contact from "../../components/Contact/Contact";
 import Footer from "../../components/Footer/Footer";
