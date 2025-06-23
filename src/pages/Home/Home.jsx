1e4fd58cb570dfac0f8cd997dbf5425104e19177
“G-Gakii” <gakiiviolet1@gmail.com>
2025-06-23 15:58:36 +0300

    max width

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -10,7 +10,7 @@ import Footer from "../../components/Footer/Footer";
 
 const Home = () => {
   return (
-    <div>
+    <div className={styles.home__container}>
       <Navbar />
       <Hero />
       <About />
